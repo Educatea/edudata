12a133c97d527b987d26ebee90034544a843904e
Juan Gesino <juangesino@gmail.com>
2015-07-21 01:22:37 -0300

    debugging

--- lib/edudata.rb
+++ lib/edudata.rb
@@ -14,73 +14,73 @@ module Edudata
   end
 
 
-  extend self
+  # extend self
 
-  def measurements
-    @measurements || = []
-  end
+  # def measurements
+  #   @measurements || = []
+  # end
 
-  def find_by_name(name)
-    self.measurements.each do |measurement|
-      if measurement.name == name
-        return measurement
-      end
-    end
-    []
-  end
+  # def find_by_name(name)
+  #   self.measurements.each do |measurement|
+  #     if measurement.name == name
+  #       return measurement
+  #     end
+  #   end
+  #   []
+  # end
 
-  def clear(token = nil, host_name = '')
+  # def clear(token = nil, host_name = '')
 
-    if ENV['HOST-NAME']
-      host_name = ENV['HOST-NAME']
-    else
-      if host_name.empty?
-        host_name = Socket.gethostname
-      end
-    end
+  #   if ENV['HOST-NAME']
+  #     host_name = ENV['HOST-NAME']
+  #   else
+  #     if host_name.empty?
+  #       host_name = Socket.gethostname
+  #     end
+  #   end
 
-    self.measurements.each do |measurement|
-      values = measurement.values
+  #   self.measurements.each do |measurement|
+  #     values = measurement.values
       
-      if token
-      	self.set_token(token)
-      else
-      	self.set_token
-      end
+  #     if token
+  #     	self.set_token(token)
+  #     else
+  #     	self.set_token
+  #     end
       
-      tag = measurement.name
-      unit = measurement.unit
+  #     tag = measurement.name
+  #     unit = measurement.unit
 
-      if values.class != Array
-        values = [values]
-      end
+  #     if values.class != Array
+  #       values = [values]
+  #     end
 
-      # uri = URI.parse("http://data.educatea.com.ar/api/v1/measurements")
-      # http = Net::HTTP.new(uri.host, uri.port)
-      # http.use_ssl = false
-      # request = Net::HTTP::Post.new(uri.request_uri)
-      # request.set_form_data({"values" => "#{values}", "token" => self.token, "tag" => tag, "host" => host_name, "unit" => unit})
-      # response = http.request(request)
-      # response = JSON.parse(response.body)
-      puts {"values" => "#{values}", "token" => self.token, "tag" => tag, "host" => host_name, "unit" => unit}
+  #     uri = URI.parse("http://data.educatea.com.ar/api/v1/measurements")
+  #     http = Net::HTTP.new(uri.host, uri.port)
+  #     http.use_ssl = false
+  #     request = Net::HTTP::Post.new(uri.request_uri)
+  #     request.set_form_data({"values" => "#{values}", "token" => self.token, "tag" => tag, "host" => host_name, "unit" => unit})
+  #     response = http.request(request)
+  #     response = JSON.parse(response.body)
+  #     puts {"values" => "#{values}", "token" => self.token, "tag" => tag, "host" => host_name, "unit" => unit}
 
-    end
+  #   end
 
-    @measurements = []
-  end
+  #   @measurements = []
+  # end
 
-  def add(name, values, unit = '')
-    if !@measurements || @measurements.empty?
-      @measurements = []
-    end
-    old_benchmark = Benchmarks.find_by_name(name)
-    if old_benchmark.class == Array
-      @measurements.push(Benchmark.new(name, values, unit))
-    else
-      old_benchmark.values = old_benchmark.values + values
-      old_benchmark.new_values
-    end
-  end
+  # def add(name, values, unit = '')
+  #   if !@measurements || @measurements.empty?
+  #     @measurements = []
+  #   end
+  #   old_benchmark = Benchmarks.find_by_name(name)
+  #   if old_benchmark.class == Array
+  #     @measurements.push(Benchmark.new(name, values, unit))
+  #   else
+  #     old_benchmark.values = old_benchmark.values + values
+  #     old_benchmark.new_values
+  #   end
+  # end
 
   class Benchmark
 
